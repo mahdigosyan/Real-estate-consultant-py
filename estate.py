83315d8d890f0e6b43277dd9cee43af02f2fc05d
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-03-11 00:41:06 +0330

    Update estate.py

--- estate.py
+++ estate.py
@@ -1,3 +1,4 @@
+from abc import abstractclassmethod
 from base import BaseClass
 
 class EsatateAbastract(BaseClass):
@@ -10,11 +11,28 @@ class EsatateAbastract(BaseClass):
         self.address=address
         super().__init__(*args, **kwargs)
 
+    @abstractclassmethod
+    def show_description(self):
+        pass
+
 class Apartment(EsatateAbastract):
-    pass
+    def __init__(self,has_elevator,has_parking,floor,*args,**kwargs):
+        self.has_elevator=has_elevator
+        self.has_parking=has_parking
+        self.floor=floor
+        super().__init__(*args,**kwargs)
+
+    def show_description(self):
+        print(f"Apartment {self.id}")
 
 class House(EsatateAbastract):
-    pass
+    def __init__(self,has_yard,floor_count,*args,**kwargs):
+        self.has_yard=has_yard
+        self.floor_count=floor_count
+        super().__init__(*args,**kwargs)
+    def show_description(self):
+        print(f"House {self.id}")
 
 class Store(EsatateAbastract):
-    pass
+    def show_description(self):
+        print(f"Store {self.id}")
